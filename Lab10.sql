9c78902c733f0d9426475184d2941219166712bb
Hannah Riedman <hannah.riedman1@marist.edu>
2017-04-15 14:05:19 -0400

    Hannah Riedman 4-18-17 Lab 10

---
+++ Lab10.sql
@@ -0,0 +1,34 @@
+﻿-- Lab 10 
+
+--1 Passed in course number, returns prereqs you need
+DROP FUNCTION prereqsfor(integer);
+create or replace function PreReqsFor(course_Num int) returns refcursor as 
+$$
+declare
+   course_Num int       := $1;
+   resultset  refcursor;
+begin
+   open resultset for 
+      select prereqnum
+      from   prerequisites
+       where  coursenum = course_Num;
+   return resultset;
+end;
+$$ language plpgsql
+
+
+-- 2 Passed in prereq, returns the courses you can take with that
+DROP FUNCTION isprereqsfor(integer);
+create or replace function IsPreReqsFor(course_Num int) returns refcursor as 
+$$
+declare
+   course_Num int       := $1;
+   resultset   refcursor;
+begin
+   open resultset for 
+      select coursenum
+      from   prerequisites
+       where  prereqnum = course_Num;
+   return resultset;
+end;
+$$ language plpgsql
